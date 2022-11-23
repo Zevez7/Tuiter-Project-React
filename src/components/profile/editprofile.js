8c285e2013833bbe143fe7ac4e87d2a21f8bab18
Zevez7 <datguy7@gmail.com>
2022-11-23 15:46:32 -0500

    edit profile page completed with dummy data

--- src/components/profile/editprofile.js
+++ src/components/profile/editprofile.js
@@ -1,12 +1,21 @@
-import { Box, TextField, Typography } from "@mui/material";
+/**
+ * @file Edit profile page
+ */
+import { Box, Button, TextField, Typography } from "@mui/material";
 import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
+/**
+ * Ediprofile component to edit user profile
+ */
 const Editprofile = () => {
+  const navigate = useNavigate();
+
   const data = {
     userId: 123455,
     profileImage:
       "www.wikipedia.org/portal/wikipedia.org/assets/img/Wikipedia-logo-v2@2x.png",
-    personalWebsite: "www.wikipedia.org/portal/wikipedia.org",
+    personalWebsite: "www.wikipedia.org",
     linkedIn: "www.linkedIn.com",
     github: "www.github.com",
     instagram: "www.instagram.com",
@@ -15,65 +24,106 @@ const Editprofile = () => {
     friends: ["12434234", "123123123", "234234234"],
   };
 
+  /**
+   * OnClick cancel button this function is called
+   */
+  const cancelBtn = () => {
+    navigate("/profile");
+  };
+
+  /**
+   * OnClick save button this function called
+   */
+  const saveBtn = () => {
+    navigate("/profile");
+  };
   return (
     <Box>
       <Typography variant="h3">Edit Profile</Typography>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Box mt={3} width={350}>
+          <TextField
+            id="outlined"
+            fullWidth
+            label="Personal Website"
+            variant="outlined"
+            defaultValue={data.personalWebsite}
+          />
+        </Box>
 
-      <Box>
-        <TextField
-          id="standard-basic"
-          label="Personal Website"
-          variant="standard"
-          defaultValue={data.personalWebsite}
-        />
+        <Box mt={3} width={350}>
+          <TextField
+            id="outlined"
+            fullWidth
+            label="LinkedIn"
+            variant="outlined"
+            defaultValue={data.linkedIn}
+          />
+        </Box>
+        <Box mt={3} width={350}>
+          <TextField
+            id="outlined"
+            fullWidth
+            label="Github"
+            variant="outlined"
+            defaultValue={data.github}
+          />
+        </Box>
+        <Box mt={3} width={350}>
+          <TextField
+            id="outlined"
+            fullWidth
+            label="Instagram"
+            variant="outlined"
+            defaultValue={data.instagram}
+          />
+        </Box>
+        <Box mt={3} width={350}>
+          <TextField
+            id="outlined"
+            fullWidth
+            label="Company"
+            variant="outlined"
+            defaultValue={data.company}
+          />
+        </Box>
+        {data.interests.map((item, key) => {
+          return (
+            <Box mt={3} width={350}>
+              <TextField
+                id="outlined"
+                fullWidth
+                label={`interest ${key + 1}`}
+                variant="outlined"
+                defaultValue={item}
+              />
+            </Box>
+          );
+        })}
+        <Box m={4}>
+          <Button
+            sx={{ width: 350 }}
+            variant="contained"
+            component="label"
+            disabled
+          >
+            Upload Profile Image
+            <input hidden accept="image/*" multiple type="file" />
+          </Button>
+        </Box>
       </Box>
-
-      <Box>
-        <TextField
-          id="standard-basic"
-          label="LinkedIn"
-          variant="standard"
-          defaultValue={data.linkedIn}
-        />
-      </Box>
-      <Box>
-        <TextField
-          id="standard-basic"
-          label="Github"
-          variant="standard"
-          defaultValue={data.github}
-        />
-      </Box>
-      <Box>
-        <TextField
-          id="standard-basic"
-          label="Instagram"
-          variant="standard"
-          defaultValue={data.instagram}
-        />
-      </Box>
-      <Box>
-        <TextField
-          id="standard-basic"
-          label="Company"
-          variant="standard"
-          defaultValue={data.company}
-        />
-      </Box>
-      {data.interests.map((item, key) => {
-        return (
-          <Box>
-            <TextField
-              id="standard-basic"
-              label={`interest ${key + 1}`}
-              variant="standard"
-              defaultValue={item}
-            />
-          </Box>
-        );
-      })}
-      <Box>
-        <TextField id="standard-basic" label="Standard" variant="standard" />
+      <Box textAlign="center" mt={4}>
+        <Button variant="outlined" sx={{ ml: 2 }} onClick={cancelBtn}>
+          Cancel
+        </Button>
+        <Button variant="outlined" sx={{ ml: 2 }} onClick={saveBtn}>
+          Save
+        </Button>
       </Box>
     </Box>
   );
