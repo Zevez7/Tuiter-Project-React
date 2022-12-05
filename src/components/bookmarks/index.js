8d4ae007287518922fd0970135c4d4b0aa557ed8
ujjvalksharma <ujjvalksharma@gmail.com>
2022-12-05 15:28:40 -0500

    display static tuit in bookmark

--- src/components/bookmarks/index.js
+++ src/components/bookmarks/index.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import tuitsArray from "../tuits/tuits-data.json"
+import Bookmark from "./bookmark";
 
 function Bookmarks () {
+
+  const [bookmarkedTuits, setBookmarkedTuits]= useState([]);
+
+useEffect(() => {
+  //fetch Tuits from an api
+  setBookmarkedTuits(tuitsArray);
+
+}, [bookmarkedTuits]);
+
   return(
+    <>
     <div>
-        <h1>Bookmarks Screen</h1>
+    {tuitsArray.map((tuits, index) => (
+    index%2===0
+      ? (<div class="row">
+      <div class="col-6"  ><Bookmark  key={'bookmark-'+index} bookmarkTuit={bookmarkedTuits[index]} /></div>
+      {index+1<tuitsArray.length?<div class="col-6" ><Bookmark key={'bookmark-'+index+1} bookmarkTuit={bookmarkedTuits[index+1]} /></div>:null}
+         </div>)
+      : null
+  ))}
     </div>
+    </>
+
   );
 }
 export default Bookmarks;
